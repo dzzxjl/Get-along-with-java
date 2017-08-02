4653170d92999d6da9cccae98b5687941f21ef3c
dzzxjl <dzzxjl@126.com>
2017-08-02 16:52:42 +0800

    测试泛型特性

--- src/main/java/com/dzzxjl/lock/CBTest.java
+++ src/main/java/com/dzzxjl/lock/CBTest.java
@@ -4,9 +4,9 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class CBTest {
-    static class Writer extends Thread{
+    static class AnotherWriter implements Runnable {
         private CyclicBarrier cyclicBarrier;
-        public Writer(CyclicBarrier cyclicBarrier) {
+        public AnotherWriter(CyclicBarrier cyclicBarrier) {
             this.cyclicBarrier = cyclicBarrier;
         }
 
@@ -16,13 +16,13 @@ public class CBTest {
             try {
                 Thread.sleep(5000);
                 System.out.println("线程" + Thread.currentThread().getName() + "写入数据完毕");
+                //单一线程执行完写数据操作后，在此等待，当所有线程均运行到此步骤后，开始执行接下来行为
                 cyclicBarrier.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (BrokenBarrierException e) {
                 e.printStackTrace();
             }
-
             System.out.println("所有线程写入完毕，继续处理其他任务...");
         }
     }
@@ -31,7 +31,9 @@ public class CBTest {
         int N = 4;
         CyclicBarrier cyclicBarrier = new CyclicBarrier(4);
         for(int i = 0;i < N; i++) {
-            new Writer(cyclicBarrier).start();
+
+//            new AnotherWriter(cyclicBarrier).start();
+            new Thread(new AnotherWriter(cyclicBarrier)).start();
         }
     }
 }
