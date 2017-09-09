6ebcdcf43b174f051869c6b5795a490876c369a4
dzzxjl <dzzxjl@126.com>
2017-09-09 09:01:09 +0800

    update文件IO

---
+++ src/main/java/com/dzzxjl/文件IO.java
@@ -0,0 +1,30 @@
+package com.dzzxjl;
+
+import java.io.*;
+
+public class 文件IO {
+    public static void main(String[] args) throws IOException{
+        // IO源
+        File file = new File("/Users/dzzxjl/Desktop/run.sh");
+        try {
+            // 水管
+            FileReader fileReader = new FileReader(file);
+            // 水池缓冲器
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            //临时存取读取的每一行的值
+            String temp;
+            while ( (temp = bufferedReader.readLine()) != null) {
+                System.out.println(temp);
+            }
+            /*
+            关闭操作
+             */
+            bufferedReader.close();
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
